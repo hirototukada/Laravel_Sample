3008ef32cccdb2d62f6c58e5c2b754c63e7f7cdb
tukada <soranohoshi739@gmail.com>
2022-03-12 15:18:26 +0900

    Mapの変更

--- resources/views/Maps/map.blade.php
+++ resources/views/Maps/map.blade.php
@@ -23,26 +23,6 @@
     <div id="app">
         <div id="map" class="bg-dark" style="height:500px">
         </div>
-        <script>
-window.onload=function initMap(){
-    console.log('egutisan');
-    // googleMapsAPIを持ってくるときに,callback=initMapと記述しているため、initMap関数を作成
-    // welcome.blade.phpで描画領域を設定するときに、id=mapとしたため、その領域を取得し、mapに格納します。
-    map = document.getElementById("map");
-    // 東京タワーの緯度は35.6585769,経度は139.7454506と事前に調べておいた
-    let tokyoTower = {lat: 35.6585769, lng: 139.7454506};
-    // オプションを設定
-    opt = {
-        zoom: 13, //地図の縮尺を指定
-        center: tokyoTower, //センターを東京タワーに指定
-    };
-    // 地図のインスタンスを作成します。第一引数にはマップを描画する領域、第二引数にはオプションを指定
-    mapObj = new google.maps.Map(map, opt);
-
-};
-
-www
-        </script>
         {{-- <script src="{{ asset('js/result.js') }}" defer></script> --}}
         <script src="https://maps.googleapis.com/maps/api/js?language=ja&region=JP&key=AIzaSyC43r8ZkHSn_2lphwvdzlGx3qdc6QKf--w&callback=initMap" async defer>
         </script>
